d93b5ae45532c94dab8b2df8395b5c07dba6cbf1
soukabe <lthioub@gmail.com>
2014-10-16 21:38:35 +0000

    32ou64bits.bat permet de connaitre l'architecture de mon systeme d exploitation

---
+++ 32ou64bits.bat
@@ -0,0 +1,7 @@
+@echo off
+Title Systeme 32 bit ou 64 bits
+color 1a
+
+if %processor_architecture% == AMD64 echo Nous avons un système Windows de 64 bits.
+if %processor_architecture% == x86 echo Nous avons un système Windows 32 bits.
+pause>nul
